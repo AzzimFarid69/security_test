558b1286d4b1c08c694d39ab2407ea49d1d59fc5
Nurhazirah <73669071+nhzrh@users.noreply.github.com>
2021-09-22 16:03:40 +0800

    Update constant file to prevent merge conflict

--- lib/common/utils/constant.dart
+++ lib/common/utils/constant.dart
@@ -6,75 +6,55 @@ import 'package:security_test/models/payment_history_model.dart';
 class Constants {
   static const skEmail = 'email';
   static const skPassword = 'password';
-  static const double padding = 20;
-  static const double avatarRadius = 45;
   static List<GeneralModel> getMonthModel() => [
         GeneralModel(
-          name: 'January',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-        ),
+            name: 'January',
+            description:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit'),
         GeneralModel(
-          name: 'February',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-        ),
+            name: 'February',
+            description:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit'),
         GeneralModel(
-          name: 'March',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-        ),
+            name: 'March',
+            description:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit'),
         GeneralModel(
-          name: 'April',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-        ),
+            name: 'April',
+            description:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit'),
         GeneralModel(
-          name: 'May',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-        ),
+            name: 'May',
+            description:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit'),
         GeneralModel(
-          name: 'June',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-        ),
+            name: 'June',
+            description:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit'),
         GeneralModel(
-          name: 'July',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-        ),
+            name: 'July',
+            description:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit'),
         GeneralModel(
-          name: 'August',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-        ),
+            name: 'August',
+            description:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit'),
         GeneralModel(
-          name: 'September',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-        ),
+            name: 'September',
+            description:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit'),
         GeneralModel(
-          name: 'October',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-        ),
+            name: 'October',
+            description:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit'),
         GeneralModel(
-          name: 'November',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-        ),
+            name: 'November',
+            description:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit'),
         GeneralModel(
-          name: 'December',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-        ),
-      ];
-  static List<PaymentHistoryModel> getPayHistory() => [
-        PaymentHistoryModel(
-          '18-07-2017 06:27:28 PM',
-          'ulp100370',
-          '1707180004058984',
-          '15,000.00',
-          '1',
-          'Immediate Bill Payment',
-        ),
-        PaymentHistoryModel(
-          '19-07-2017 06:30:28 PM',
-          'ulp100371',
-          '1707180004058985',
-          '15,000.00',
-          '2',
-          'Transfer Funds',
-        )
+            name: 'December',
+            description:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit'),
       ];
 
   static List<OBForexRateData> retrieveExchangeRate() => [
@@ -161,6 +141,27 @@ class Constants {
         ObFixedDepositRate(currency: 'USD', tenure: '2', rate: '2.95'),
         ObFixedDepositRate(currency: 'USD', tenure: '3', rate: '3.00'),
         ObFixedDepositRate(currency: 'USD', tenure: '6', rate: '3.25'),
+      ];
+
+  static const double padding = 20;
+  static const double avatarRadius = 45;
+  static List<PaymentHistoryModel> getPayHistory() => [
+        PaymentHistoryModel(
+          '18-07-2017 06:27:28 PM',
+          'ulp100370',
+          '1707180004058984',
+          '15,000.00',
+          '1',
+          'Immediate Bill Payment',
+        ),
+        PaymentHistoryModel(
+          '19-07-2017 06:30:28 PM',
+          'ulp100371',
+          '1707180004058985',
+          '15,000.00',
+          '2',
+          'Transfer Funds',
+        )
       ];
 
   // Value Formatting //
